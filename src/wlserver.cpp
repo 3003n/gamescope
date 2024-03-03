6dd63fa66afbd389d51a3d17952a33426a7674ae
honjow <honjow311@gamil.com>
2024-03-04 05:50:40 +0800

    Fix internal display touchscreen orientation when it's forced

--- src/wlserver.cpp
+++ src/wlserver.cpp
@@ -2019,8 +2019,12 @@ static void apply_touchscreen_orientation(double *x, double *y )
 	double tx = 0;
 	double ty = 0;
 
-	// Use internal screen always for orientation purposes.
-	switch ( GetBackend()->GetConnector( gamescope::GAMESCOPE_SCREEN_TYPE_INTERNAL )->GetCurrentOrientation() )
+	auto orientation = GAMESCOPE_PANEL_ORIENTATION_AUTO;
+	if ( GetBackend() && GetBackend()->GetCurrentConnector(  ) )
+	{
+		orientation = GetBackend()->GetCurrentConnector()->GetCurrentOrientation();
+	}
+	switch ( orientation )
 	{
 		default:
 		case GAMESCOPE_PANEL_ORIENTATION_AUTO:
@@ -2053,9 +2057,13 @@ int get_effective_touch_mode()
 	if ( !GetBackend() || !GetBackend()->GetCurrentConnector() )
 		return g_nTouchClickMode;
 
-	gamescope::GamescopeScreenType screenType = GetBackend()->GetCurrentConnector()->GetScreenType();
-	if ( screenType == gamescope::GAMESCOPE_SCREEN_TYPE_EXTERNAL && g_nTouchClickMode == WLSERVER_TOUCH_CLICK_PASSTHROUGH )
-		return WLSERVER_TOUCH_CLICK_TRACKPAD;
+	// auto b_DisplayTypeInternal = GetBackend()->GetCurrentConnector()->DisplayTypeInternal();
+
+	// gamescope::GamescopeScreenType screenType = GetBackend()->GetCurrentConnector()->GetScreenType();
+	// if ( screenType == gamescope::GAMESCOPE_SCREEN_TYPE_EXTERNAL && 
+	// 		g_nTouchClickMode == WLSERVER_TOUCH_CLICK_PASSTHROUGH && 
+	// 		!b_DisplayTypeInternal )
+	// 	return WLSERVER_TOUCH_CLICK_TRACKPAD;
 
 	return g_nTouchClickMode;
 }
