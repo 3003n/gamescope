3903caadbab945a86860cba89e98194e6d1f54ff
Autumn Ashton <misyl@froggi.es>
2025-03-01 22:35:56 +0000

    steamcompmgr: Fix icon/title being spam set
    Overlays should not get an appid, that's just for focus id logic.

--- src/steamcompmgr.cpp
+++ src/steamcompmgr.cpp
@@ -3961,8 +3961,12 @@ determine_and_apply_focus()
 		if ( global_focus.focusWindow )
 		{
 			GetBackend()->GetNestedHints()->SetVisible( true );
-			GetBackend()->GetNestedHints()->SetTitle( global_focus.focusWindow->title );
-			GetBackend()->GetNestedHints()->SetIcon( global_focus.focusWindow->icon );
+			if ( global_focus.focusWindow != previous_focus.focusWindow )
+			{
+				// Set title and icon for the focused window
+				GetBackend()->GetNestedHints()->SetTitle( global_focus.focusWindow->title );
+				GetBackend()->GetNestedHints()->SetIcon( global_focus.focusWindow->icon );
+			}
 		}
 		else
 		{
@@ -4200,8 +4204,14 @@ map_win(xwayland_ctx_t* ctx, Window id, unsigned long sequence)
 	{
 		w->appID = w->xwayland().id;
 	}
+	
 	w->isOverlay = get_prop(ctx, w->xwayland().id, ctx->atoms.overlayAtom, 0);
 	w->isExternalOverlay = get_prop(ctx, w->xwayland().id, ctx->atoms.externalOverlayAtom, 0);
+
+	// misyl: Disable appID for overlay types, as parts of the code don't expect that focus-wise.
+	// Fixes mangoapp usage when nested, and not in SteamOS.
+	if ( w->IsAnyOverlay() )
+		w->appID = 0;
 
 	get_size_hints(ctx, w);
 
@@ -4462,6 +4472,9 @@ add_win(xwayland_ctx_t *ctx, Window id, Window prev, unsigned long sequence)
 		new_win->appID = id;
 	}
 
+	if ( new_win->IsAnyOverlay() )
+		new_win->appID = 0;
+
 	Window transientFor = None;
 	if ( XGetTransientForHint( ctx->dpy, id, &transientFor ) )
 	{
@@ -4693,7 +4706,7 @@ damage_win(xwayland_ctx_t *ctx, XDamageNotifyEvent *de)
 	if (!w)
 		return;
 
-	if ((w->isOverlay || w->isExternalOverlay) && !w->opacity)
+	if (w->IsAnyOverlay() && !w->opacity)
 		return;
 
 	// First damage event we get, compute focus; we only want to focus damaged
@@ -5295,6 +5308,8 @@ handle_property_notify(xwayland_ctx_t *ctx, XPropertyEvent *ev)
 				xwm_log.errorf( "appid clash was %u now %u", w->appID, appID );
 			}
 			w->appID = appID;
+			if ( w->IsAnyOverlay() )
+				w->appID = 0;
 
 			MakeFocusDirty();
 		}
@@ -5305,6 +5320,8 @@ handle_property_notify(xwayland_ctx_t *ctx, XPropertyEvent *ev)
 		if (w)
 		{
 			w->isOverlay = get_prop(ctx, w->xwayland().id, ctx->atoms.overlayAtom, 0);
+			if ( w->IsAnyOverlay() )
+				w->appID = 0;
 			MakeFocusDirty();
 		}
 	}
@@ -5314,6 +5331,8 @@ handle_property_notify(xwayland_ctx_t *ctx, XPropertyEvent *ev)
 		if (w)
 		{
 			w->isExternalOverlay = get_prop(ctx, w->xwayland().id, ctx->atoms.externalOverlayAtom, 0);
+			if ( w->IsAnyOverlay() )
+				w->appID = 0;
 			MakeFocusDirty();
 		}
 	}
