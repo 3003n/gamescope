0c51970751b76fc2ba62f974d11c0b3417b46501
honjow <honjow311@gmail.com>
2025-11-17 20:08:05 +0800

    fix: improve backend framebuffer handling in DRMBackend
    Refactor framebuffer retrieval to ensure proper handling of null pointers. This change enhances stability by preventing potential dereferencing of null pointers when accessing backend framebuffers.

--- src/Backends/DRMBackend.cpp
+++ src/Backends/DRMBackend.cpp
@@ -2560,7 +2560,8 @@ drm_prepare_liftoff( struct drm_t *drm, const struct FrameInfo_t *frameInfo, boo
 		if ( i < frameInfo->layerCount )
 		{
 			const FrameInfo_t::Layer_t *pLayer = &frameInfo->layers[ i ];
-			gamescope::CDRMFb *pDrmFb = static_cast<gamescope::CDRMFb *>( pLayer->tex ? pLayer->tex->GetBackendFb()->Unwrap() : nullptr );
+			auto pBackendFb = pLayer->tex ? pLayer->tex->GetBackendFb() : nullptr;
+			gamescope::CDRMFb *pDrmFb = pBackendFb ? static_cast<gamescope::CDRMFb *>( pBackendFb->Unwrap() ) : nullptr;
 
 			if ( pDrmFb == nullptr )
 			{
